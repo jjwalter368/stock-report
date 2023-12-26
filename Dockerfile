4847625984e746cd1698b4f07e4070d2b2a23776
Joshua Walter <jjw368@gmail.com>
2023-12-25 21:27:09 -0500

    added yfinance module, iteration through the watchlist, and a try-except loop. STABLE (it works good enough for now)

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM python:3.11-slim
 WORKDIR /app
 
 COPY ./requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir yfinance
 COPY ./src ./src
 
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
-def run():
-    print("Running... ... ...")
-    print("Is is working?")
-    yn = input()
-    print(yn)
+import yfinance as yf
+import json
 
-run()
+def main():
+    print("Welcome to Stock Report v1.0.0 by Joshua Walter\n\n")
+    watchlist = input("Please Enter The List Of Stocks You Want A Report On (tickers seperated by spaces):\n").split()
+    #look for going through each ticker (run many different functions to pull data and compile report)
+    for x in watchlist:
+        try:
+            stock = yf.Ticker(x).info
+            currentPrice = stock["currentPrice"]
+            lastClose = stock["regularMarketPreviousClose"]
+            dollarChange = currentPrice-lastClose
+            percentChange = round((((currentPrice - lastClose)/lastClose) * 100), 2)
+            #make sure to add ratios, book values, 200 day average, etc. (financial indicators and stats that I use)
+            #maybe try to pull some news or get a positive/negative news indicator??
+            recommendationMean = stock["recommendationMean"]
+            recommendationKey = stock["recommendationKey"]
+            print("Ticker: " + x + "\n")
+            print("Current Price: " + str(currentPrice) + "\n\n")
+            print("Last Close: " + str(lastClose) + "\n\n")
+            print("Dollar Change: " + str(dollarChange) + "\n\n")
+            print("Percent Change: " + str(percentChange) + "\n\n")
+            print("Recommendations Average (1-5): \n" + str(recommendationMean) + "\n\n")
+            print("Recommendations Key: \n" + recommendationKey + "\n\n")
+        except:
+            print("Error: Possible incorrect ticker or code error") 
+        
+
+main()
