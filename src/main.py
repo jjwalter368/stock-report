9bebd3131df8e25d38e70bad355be1099999f7fd
Joshua Walter <jjw368@gmail.com>
2023-12-26 22:26:05 -0500

    added some financial indicators and ratios STABLE

--- src/main.py
+++ src/main.py
@@ -7,22 +7,35 @@ def main():
     #look for going through each ticker (run many different functions to pull data and compile report)
     for x in watchlist:
         try:
+            #remember to see if there is any more financial indicators you need
             stock = yf.Ticker(x).info
             currentPrice = stock["currentPrice"]
             lastClose = stock["regularMarketPreviousClose"]
             dollarChange = currentPrice-lastClose
             percentChange = round((((currentPrice - lastClose)/lastClose) * 100), 2)
-            #make sure to add ratios, book values, 200 day average, etc. (financial indicators and stats that I use)
-            #maybe try to pull some news or get a positive/negative news indicator??
+            priceToBook = stock["priceToBook"]
+            trailingPriceToEarnings = stock["trailingPE"]
+            forwardPriceToEarnings = stock["forwardPE"]
+            fiftyDayMA = stock["fiftyDayAverage"]
+            twoHundredDayMA = stock["twoHundredDayAverage"]
+            dividendYield = stock["dividendYield"] * 100
             recommendationMean = stock["recommendationMean"]
             recommendationKey = stock["recommendationKey"]
+            print("-"*20)
             print("Ticker: " + x + "\n")
             print("Current Price: " + str(currentPrice) + "\n\n")
             print("Last Close: " + str(lastClose) + "\n\n")
             print("Dollar Change: " + str(dollarChange) + "\n\n")
-            print("Percent Change: " + str(percentChange) + "\n\n")
+            print("Percent Change: " + str(percentChange) + "%\n\n")
+            print("Price To Book: " + str(priceToBook) + "\n\n")
+            print("Trailing Price To Earnings: " + str(trailingPriceToEarnings) + "\n\n")
+            print("Forward Price To Earnings: " + str(forwardPriceToEarnings) + "\n\n")
+            print("50 Day MA: " + str(fiftyDayMA) + "\n\n")
+            print("200 Day MA: " + str(twoHundredDayMA) + "\n\n")
+            print("Dividend Yield: " + str(dividendYield) + "%\n\n")
             print("Recommendations Average (1-5): \n" + str(recommendationMean) + "\n\n")
             print("Recommendations Key: \n" + recommendationKey + "\n\n")
+            print("-"*20)
         except:
             print("Error: Possible incorrect ticker or code error") 
         
