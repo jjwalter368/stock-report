ee293c953457e006a88a03a929a8c01f6aabb936
Joshua Walter <jjw368@gmail.com>
2024-01-06 16:22:07 -0500

    orginization (changing the main runction into multiple functions for reabability and debugging), and added comments for easily adding more changes later

--- src/main.py
+++ src/main.py
@@ -28,8 +28,65 @@ def clsOrient():
     #print("\n" * int((round(os.get_terminal_size().lines)/2, 0)))
     print("\n" * heightMargins)
 
+def pullInfo(ticker, dictionary):
+        try:
+            #develop a function for pulling the info for a stock (intakes the ticker and the dictionary so it can update it)
+            stock = yf.Ticker(ticker).info
+            dict["Ticker"].append(ticker)
+            try:
+                currentPrice = stock["currentPrice"]
+                dict["Current Price"].append(str(currentPrice))
+            except:
+                dict["Current Price"].append("N/A")
+            try:
+                lastClose = stock["regularMarketPreviousClose"]
+                dict["Last Close"].append(str(lastClose))
+            except:
+                dict["Last Close"].append("N/A")
+            try:
+                dollarChange = round(currentPrice-lastClose, 2)
+                dict["Dollar Change"].append("$" + str(dollarChange))
+            except:
+                dict["Dollar Change"].append("N/A")
+            try:
+                percentChange = round((((currentPrice - lastClose)/lastClose) * 100), 2)
+                dict["Percent Change"].append(str(percentChange) + "%")
+            except:
+                dict["Percent Change"].append("N/A")
+            try:
+                priceToBook = stock["priceToBook"]
+                dict["P/B"].append(str(priceToBook))
+            except:
+                dict["P/B"].append("N/A")
+            try:
+                trailingPriceToEarnings = stock["trailingPE"]
+                dict["Trailing P/E"].append(str(trailingPriceToEarnings))
+            except:
+                dict["Trailing P/E"].append("N/A")
+            try:
+                forwardPriceToEarnings = stock["forwardPE"]
+                dict["Forward P/E"].append(str(forwardPriceToEarnings))
+            except:
+                dict["Forward P/E"].append("N/A")
+            try:
+                fiftyDayMA = stock["fiftyDayAverage"]
+                dict["50 MA"].append(str(fiftyDayMA))
+            except:
+                dict["50 MA"].append("N/A")
+            try:
+                twoHundredDayMA = stock["twoHundredDayAverage"]
+                dict["200 MA"].append(str(twoHundredDayMA))
+            except:
+                dict["200 MA"].append("N/A")
+        except Exception as error:
+            print(error + "\n") 
+            print(ticker)
+
 def main():
-    #look for going through each ticker (run many different functions to pull data and compile report)
+    global dict
+    #until exited, initialize a blank-slate dictionary, then assign the variables pulled from yahoo finance, then create the data frame...
+    #clear the screen, add the vertical margins, then print the table while adding the horizontal margins, with an exception clause ...
+    #Then wait 5 seconds and clear the screen and print the verticle margins again
     while True:
         dict = {
                 'Ticker': [],
@@ -44,87 +101,20 @@ def main():
                 '200 MA': [],
         }
         for x in watchlist:
-            try:
-                #remember to see if there is any more financial indicators you need
-                stock = yf.Ticker(x).info
-                dict["Ticker"].append(x)
-                try:
-                    currentPrice = stock["currentPrice"]
-                    dict["Current Price"].append(str(currentPrice))
-                except:
-                    dict["Current Price"].append("N/A")
-                try:
-                    lastClose = stock["regularMarketPreviousClose"]
-                    dict["Last Close"].append(str(lastClose))
-                except:
-                    dict["Last Close"].append("N/A")
-                try:
-                    dollarChange = round(currentPrice-lastClose, 2)
-                    dict["Dollar Change"].append("$" + str(dollarChange))
-                except:
-                    dict["Dollar Change"].append("N/A")
-                try:
-                    percentChange = round((((currentPrice - lastClose)/lastClose) * 100), 2)
-                    dict["Percent Change"].append(str(percentChange) + "%")
-                except:
-                    dict["Percent Change"].append("N/A")
-                try:
-                    priceToBook = stock["priceToBook"]
-                    dict["P/B"].append(str(priceToBook))
-                except:
-                    dict["P/B"].append("N/A")
-                try:
-                    trailingPriceToEarnings = stock["trailingPE"]
-                    dict["Trailing P/E"].append(str(trailingPriceToEarnings))
-                except:
-                    dict["Trailing P/E"].append("N/A")
-                try:
-                    forwardPriceToEarnings = stock["forwardPE"]
-                    dict["Forward P/E"].append(str(forwardPriceToEarnings))
-                except:
-                    dict["Forward P/E"].append("N/A")
-                try:
-                    fiftyDayMA = stock["fiftyDayAverage"]
-                    dict["50 MA"].append(str(fiftyDayMA))
-                except:
-                    dict["50 MA"].append("N/A")
-                try:
-                    twoHundredDayMA = stock["twoHundredDayAverage"]
-                    dict["200 MA"].append(str(twoHundredDayMA))
-                except:
-                    dict["200 MA"].append("N/A")
-
-                #legacy version for just printing the information out into console
-                #print("-"*20)
-                #print("Ticker: " + x + "\n")
-                #print("Current Price: " + str(currentPrice) + "\n\n")
-                #print("Last Close: " + str(lastClose) + "\n\n")
-                #print("Dollar Change: " + str(dollarChange) + "\n\n")
-                #print("Percent Change: " + str(percentChange) + "%\n\n")
-                #print("Price To Book: " + str(priceToBook) + "\n\n")
-                #print("Trailing Price To Earnings: " + str(trailingPriceToEarnings) + "\n\n")
-                #print("Forward Price To Earnings: " + str(forwardPriceToEarnings) + "\n\n")
-                #print("50 Day MA: " + str(fiftyDayMA) + "\n\n")
-                #print("200 Day MA: " + str(twoHundredDayMA) + "\n\n")
-                #print("Dividend Yield: " + str(dividendYield) + "%\n\n")
-                #print("Recommendations Average (1-5): \n" + str(recommendationMean) + "\n\n")
-                #print("Recommendations Key: \n" + recommendationKey + "\n\n")
-                #print("-"*20)
-            except:
-                print("Error: Possible incorrect ticker or code error") 
-                print(x)
+            pullInfo(x, dict)
         try:
             df = pd.DataFrame(dict)
             clsOrient()
-            #add a width margin by splitting the tabulate string by '\n' and adding half the width of the screen minus the width of the box
-            #151 chars
+            #print the width margins in 
             for x in tabulate(df, headers = 'keys', tablefmt = 'psql', stralign='center').split("\n"):
                 print(widthMargins*" " + x)
         except Exception as error:
+            #Throws error and dumps the dict data for debugging purposes
             jsondict = json.dumps(dict, indent=4)
             print(jsondict)
             print(error)
-        time.sleep(10)
+        #Waits 5 seconds then clears the terminal. measures the size of the window, and adds the verticle margin
+        time.sleep(5)
         clsOrient()
 
 main()
